f4b16e079058b8c80eddd703dd060d3209364d6b
jajupmochi <jajupmochi@gmail.com>
2022-03-08 15:50:00 +0100

    [Feature] Add the function to convert smiles to networkx graphs.

--- dataset/format_dataset.py
+++ dataset/format_dataset.py
@@ -8,14 +8,46 @@ Created on Fri Dec  3 14:48:39 2021
 import numpy as np
 
 
+def to_nxgraphs(data, ds_name, verbose=True, **kwargs):
+	data_tmp = to_smiles(data, ds_name, **kwargs)
+
+	# Convert smiles to nxgraphs.
+	ds_size = len(data_tmp['X'])
+	X, idx_true = [], []
+	for i in range(ds_size):
+		try:
+			g = smiles2nxgraph_rdkit(data_tmp['X'][i])
+		except AttributeError:
+			pass
+		else:
+			X.append(g)
+			idx_true.append(i)
+
+	if verbose and (ds_size != len(idx_true)):
+		print('%d graph(s) are removed due to unrecognized smiles.' %
+			(ds_size - len(idx_true)))
+
+	# Save nxgraphs to data.
+	data = {'X': X}
+	for k in data_tmp.keys():
+		if k != 'X' and len(data_tmp['X']) == len(data_tmp[k]):
+			data[k] = [data_tmp[k][i] for i in idx_true]
+
+	return data
+
+
 def to_smiles(data, ds_name, **kwargs):
 	if ds_name == 'thermophysical':
-		t_type = (kwargs['t_type'] if 't_type' in kwargs else 'cal')
-		rm_replicate = (kwargs['rm_replicate'] if 'rm_replicate' in kwargs else True)
+		t_type = kwargs.get('t_type', 'cal')
+		rm_replicate = kwargs.get('rm_replicate', True)
 		return thermophysical_to_smiles(data, t_type=t_type, rm_replicate=rm_replicate)
 
 	elif ds_name == 'polyacrylates200':
-		return polyacrylates200_to_smiles(data)
+		with_names = kwargs.get('with_names', False)
+		return polyacrylates200_to_smiles(data, with_names=with_names)
+
+	elif ds_name.lower() == 'sugarmono':
+		return data
 
 
 def thermophysical_to_smiles(data, t_type='cal', rm_replicate=True):
@@ -52,11 +84,72 @@ def thermophysical_to_smiles(data, t_type='cal', rm_replicate=True):
 	return dataset
 
 
-def polyacrylates200_to_smiles(data):
+def polyacrylates200_to_smiles(data, with_names=False):
 
-	dataset = {'X': [], 'targets': []}
+	dataset = {}
 
 	dataset['X'] = [i.replace(' ', '') for i in data.iloc[:, 3].to_list()]
 	dataset['targets'] = [float(i) for i in data.iloc[:, 2].to_list()]
+	if with_names:
+		dataset['names'] = [i.strip() for i in data.iloc[:, 1].to_list()]
 
 	return dataset
+
+
+#%%
+
+
+def smiles2nxgraph_rdkit(smiles_string):
+	"""Converts SMILES string to NetworkX graph object by RDKit library.
+
+	Parameters
+	----------
+	smiles_string : string
+		SMILES string.
+
+	Returns
+	-------
+	Graph object.
+	"""
+	from rdkit import Chem
+	mol = Chem.MolFromSmiles(smiles_string)
+	mol = mol_to_nx(mol)
+	return mol
+
+
+def mol_to_nx(mol):
+	"""Converts RDKit mol to NetworkX graph object.
+
+	Parameters
+	----------
+	mol : RDKit mol
+		RDKit molecule.
+
+	Returns
+	-------
+	G : Networkx Graph.
+		Graph object.
+
+	References
+	----------
+	`keras-molecules <https://github.com/dakoner/keras-molecules/blob/dbbb790e74e406faa70b13e8be8104d9e938eba2/convert_rdkit_to_networkx.py#L17>`__
+	"""
+	import networkx as nx
+	G = nx.Graph()
+
+	for atom in mol.GetAtoms():
+		G.add_node(atom.GetIdx(),
+			       symbol=atom.GetSymbol(),
+# 				   atomic_num=atom.GetAtomicNum(),
+# 				   formal_charge=atom.GetFormalCharge(),
+# 				   chiral_tag=atom.GetChiralTag(),
+# 				   hybridization=atom.GetHybridization(),
+# 				   num_explicit_hs=atom.GetNumExplicitHs(),
+# 				   is_aromatic=atom.GetIsAromatic()
+				   )
+	for bond in mol.GetBonds():
+		G.add_edge(bond.GetBeginAtomIdx(),
+				   bond.GetEndAtomIdx(),
+   				   bond_type_double=str(bond.GetBondTypeAsDouble()))
+# 				   bond_type=bond.GetBondType())
+	return G
