91bd1d1538d87a7d22ae0a4d1d13e8fda7508a33
jajupmochi <jajupmochi@gmail.com>
2023-09-27 18:28:12 +0200

    [Features] Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='redox_prediction',
+	version='1.0.0',
+	packages=[''],
+	url='https://github.com/jajupmochi/RedoxPrediction',
+	license='',
+	author='linlin',
+	author_email='linlin.jia@unibe.ch',
+	description='The codes for the RedoxPrediction project.'
+)
